694c52f82e613b18f29971d52904da5ab38b57b6
Vamsi Krishna Paladugu <vamsidevelops@gmail.com>
2024-11-12 14:27:52 -0500

    Add project files.

---
+++ Motor Vehicle Crash Report/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Motor_Vehicle_Crash_Report.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
